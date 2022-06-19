cf0b5607b6b2d8f18e83cf64cf6f7cc2e09af5b8
guntza10 <jomphop.saibuatong@hotmail.com>
2022-06-19 16:50:20 +0700

    css & class directive

--- svelte-tutorial/src/components/Announcement.svelte
+++ svelte-tutorial/src/components/Announcement.svelte
@@ -1,5 +1,6 @@
 <script>
   let show = true;
+  let warning = false;
 
   const hideAlert = () => {
     show = false;
@@ -7,7 +8,12 @@
 </script>
 
 {#if show}
-  <div class="alert">
+  <!-- css & class directive -->
+  <!-- css ที่ถูกเขียนใน component จะใช้ได้แค่ใน component นั้นๆเท่านั้น ถ้าอยากให้ใช้ได้ทุก component ให้ไปเขียนที่ global.css -->
+  <!-- class directive => class:ชื่อ class ={true/false} -->
+  <!-- <div class="alert" class:warning={warning}> -->
+  <!-- shorthand class directive ถ้ามันชื่อเดียวกับตัวแปรที่เราเอามา bind -->
+  <div class="alert" class:warning>
     50 % off all fruit. sign up for more fruit!
     <button on:click={hideAlert}>Hide</button>
   </div>
@@ -19,5 +25,10 @@
     font-size: 18px;
     background-color: darkgreen;
     color: darkslategray;
+  }
+
+  .warning {
+    background-color: lightpink;
+    color: darkred;
   }
 </style>
