165cdaa35178ac30b9ab78ff6846a684b14b135b
guntza10 <jomphop.saibuatong@hotmail.com>
2022-06-19 15:47:34 +0700

    init project svelte tutorial

---
+++ svelte-tutorial/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import VariablesAndStyling from "./VariablesAndStyling.svelte";
+  import HandlingInputs from "./HandlingInputs.svelte";
+  import ReactiveValues from "./ReactiveValues.svelte";
+  import Loop from "./Loop.svelte";
+  import InlineEventHandle from "./InlineEventHandle.svelte";
+</script>
+
+<main>
+  <VariablesAndStyling />
+  <HandlingInputs />
+  <ReactiveValues />
+  <Loop />
+  <InlineEventHandle />
+</main>
+
+<style>
+</style>
